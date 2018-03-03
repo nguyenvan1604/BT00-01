aaa2733efd04c9437d9ddda5f667c6a099c086f6
Nguyễn Ngọc Văn <xoxo2team@gmail.com>
2018-03-03 19:39:39 +0700

    Mang 2 chieu

--- BT00_2_1.c
+++ BT00_2_1.c
@@ -9,6 +9,8 @@ int main()
     int i,j;
     int sum,sum1,sum2;
     int flag;
+
+    //Nhap ma tran
     printf("Nhap ma tran:\n");
     for(i=0;i<size;i++)
         for(j=0;j<size;j++)
@@ -17,12 +19,23 @@ int main()
             scanf("%d",p+i*size+j);
         }
 
+    //In ra ma tran vua nhap
+    printf("Ma tran vua nhap:\n");
+    for(i=0;i<size;i++)
+        for(j=0;j<size;j++)
+        {
+            printf("%3d",*(p+i*size+j));
+            if(j==size-1)
+                printf("\n");
+        }
+
+    printf("-------------------------------");
+    //Kiem tra ma phuong
     sum = 0;
     for(i=0;i<size;i++)
         for(j=0;j<size;j++)
             if(i==j)
                 sum += *(p+i*size+j);
-
     for(i=0;i<size;i++)
     {
         sum1 = 0;
@@ -36,7 +49,6 @@ int main()
             break;
         }
     }
-
     for(i=0;i<size;i++)
     {
         sum2 = 0;
@@ -55,15 +67,18 @@ int main()
     else
         printf("\nMa tran tren khong la ma phuong");
 
+    //Tinh tong cac phan tu duong cheo
     printf("\nTong duong cheo cua ma tran la: %d",sum);
 
+
+    //Tinh tong cac phan tu ma tran
     sum = 0;
     for(i=0;i<size;i++)
         for(j=0;j<size;j++)
             sum += *(p+i*size+j);
-
     printf("\nTong tat ca phan tu cua ma tran la: %d",sum);
 
+    //Tim ma tran chuyen vi
     int temp;
     for(i=1;i<size;i++)
         for(j=0;j<i;j++)
@@ -72,11 +87,15 @@ int main()
             *(p+i*size+j) = *(p+j*size+i);
             *(p+j*size+i) = temp;
         }
-
     printf("\nMa tran chuyen vi cua ma tran tren la:\n");
     for(i=0;i<size;i++)
         for(j=0;j<size;j++)
-            printf("%2d\n",*(p+i*size+j));
+        {
+            printf("%3d",*(p+i*size+j));
+            if(j==size-1)
+                printf("\n");
+        }
+    printf("\n\n");
 
     return 0;
 }
