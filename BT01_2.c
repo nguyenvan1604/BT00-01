89c31b74a53dc75d1cc56a0c4ded0ee28f341984
Nguyễn Ngọc Văn <xoxo2team@gmail.com>
2018-03-03 21:00:46 +0700

    Tính tổng ma trận

--- BT01_2.c
+++ BT01_2.c
@@ -3,13 +3,23 @@
  int main()
  {
     float A[2][3] = {1,2,3,4,5,6};
-    int n = sizeof(A) / sizeof(float);
     float *p = A;
-    int i;
+    printf("In ra ma tran:\n");
+    int i,j;
+    for(i=0;i<2;i++)
+        for(j=0;j<3;j++)
+        {
+            printf("%6.2f",*(p+i*2+j));
+            if(j==3-1)
+                printf("\n");
+        }
+    int n = sizeof(A) / sizeof(float);
+
     float sum = 0;
     for(i=0;i<n;i++)
         sum += *(p+i);
-    printf("%.2f", sum);
+    printf("Tong cua ma tra tren la: %.2f",sum);
+    printf("\n\n");
 
     return 0;
  }
