226e545e786b5f7bcc72a68b0a92831d6daf402c
Nguyễn Ngọc Văn <xoxo2team@gmail.com>
2018-03-04 10:53:22 +0700

    Tính tổng ma trận

--- BT01_2.c
+++ BT01_2.c
@@ -9,7 +9,7 @@
     for(i=0;i<2;i++)
         for(j=0;j<3;j++)
         {
-            printf("%6.2f",*(p+i*2+j));
+            printf("%6.2f",*(p+i*3+j));
             if(j==3-1)
                 printf("\n");
         }
